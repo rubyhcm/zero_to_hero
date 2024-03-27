97ef509af59fc97c25f515919015de4e667777b9
nguyenloi <itweb.hcm@gmail.com>
2024-03-27 23:29:46 +0700

    12_send_gmail

--- config/environments/development.rb
+++ config/environments/development.rb
@@ -38,9 +38,22 @@ Rails.application.configure do
   # config.active_storage.service = :amazon
 
   # Don't care if the mailer can't send.
-  config.action_mailer.raise_delivery_errors = false
   config.action_mailer.perform_caching = false
+
   config.action_mailer.default_url_options = { host: 'localhost', port: 3000 }
+  # config.action_mailer.default_url_options = {host: "superails-emails.herokuapp.com", protocol: "https"}
+  config.action_mailer.delivery_method = :smtp
+  config.action_mailer.perform_deliveries = true
+  config.action_mailer.raise_delivery_errors = true
+  config.action_mailer.smtp_settings = {
+    address:              'smtp.gmail.com',
+    port:                 587,
+    domain:               'gmail.com',
+    user_name:            Rails.application.credentials.dig(:google_smtp, :email),
+    password:             Rails.application.credentials.dig(:google_smtp, :password),
+    authentication:       'plain',
+    enable_starttls_auto: true
+  }
 
   # Print deprecation notices to the Rails logger.
   config.active_support.deprecation = :log
