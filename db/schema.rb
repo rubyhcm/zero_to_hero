2017d7ddd8ecd950cd2d0e7fdddb8164acdb1d36
nguyenloi <itweb.hcm@gmail.com>
2024-03-28 23:36:47 +0700

    22_using_enum

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.1].define(version: 2024_03_28_020619) do
+ActiveRecord::Schema[7.1].define(version: 2024_03_28_075547) do
   create_table "action_text_rich_texts", force: :cascade do |t|
     t.string "name", null: false
     t.text "body"
@@ -54,6 +54,7 @@ ActiveRecord::Schema[7.1].define(version: 2024_03_28_020619) do
     t.text "content"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.string "status"
   end
 
   create_table "telegrams", force: :cascade do |t|
